d5f08f944a9377fc4a49d707cf696764da9f26fb
Gustavobiz <gustavobernardes05@hotmail.com>
2025-03-11 12:17:04 -0300

    feat: server teste

---
+++ backend/server.js
@@ -0,0 +1,16 @@
+require("dotenv").config();
+const express = require("express");
+const cors = require("cors");
+
+const app = express();
+app.use(express.json());
+app.use(cors());
+
+app.get("/", (req, res) => {
+  res.send("SIG-SEG API funcionando!");
+});
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+  console.log(`Servidor rodando na porta ${PORT}`);
+});
