e05e533cc9f8cbbf6ae07437951d4c09175c1570
Gustavobiz <gustavobernardes05@hotmail.com>
2025-03-11 15:51:39 -0300

    feat: cria e atualiza ocorrencias

--- backend/routes/ocorrencias.js
+++ backend/routes/ocorrencias.js
@@ -1,6 +1,9 @@
 const express = require("express");
 const { PrismaClient } = require("@prisma/client");
-const { verificarToken, verificarServidor } = require("../middlewares/authMiddleware");
+const {
+  verificarToken,
+  verificarServidor,
+} = require("../middlewares/authMiddleware");
 
 const prisma = new PrismaClient();
 const router = express.Router();
@@ -12,8 +15,30 @@ router.post("/criar", verificarToken, verificarServidor, async (req, res) => {
 
   try {
     // Verifica a existência da denuncia
-    const denuncia = await prisma.denuncia.findUnique({ where: { id: denunciaId } });
+    const denuncia = await prisma.denuncia.findUnique({
+      where: { id: denunciaId },
+    });
 
     if (!denuncia) {
       return res.status(404).json({ error: "Denúncia não encontrada" });
     }
+    // Criar ocorrência
+    const ocorrencia = await prisma.ocorrencia.create({
+      data: {
+        denunciaId,
+        servidorId,
+        status: "aberta",
+      },
+    });
+
+    // Atualiza status
+    await prisma.denuncia.update({
+      where: { id: denunciaId },
+      data: { status: "em análise" },
+    });
+
+    res.status(201).json({ message: "Ocorrência registrada!", ocorrencia });
+  } catch (error) {
+    res.status(500).json({ error: "Erro ao criar ocorrência" });
+  }
+});
