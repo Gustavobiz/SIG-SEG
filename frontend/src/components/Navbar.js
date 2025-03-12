d6cc411eee365e77daa9d3deafe900606f3ae17b
Gustavobiz <gustavobernardes05@hotmail.com>
2025-03-12 15:33:45 -0300

    feat: teste frontend

---
+++ frontend/src/components/Navbar.js
@@ -0,0 +1,10 @@
+"use client";
+
+import Link from "next/link";
+import { useState } from "react";
+
+export default function Navbar() {
+  const [isOpen, setIsOpen] = useState(false);
+
+  return <nav>fffff</nav>;
+}
