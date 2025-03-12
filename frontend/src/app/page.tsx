def2e31d2c5706cb1728f3f47862f43656353527
Gustavobiz <gustavobernardes05@hotmail.com>
2025-03-12 11:52:02 -0300

    feat: teste frontend

---
+++ frontend/src/app/page.tsx
@@ -0,0 +1,10 @@
+export default function Home() {
+  return (
+    <div className="flex flex-col items-center justify-center min-h-screen bg-primary text-white">
+      <h1 className="text-4xl font-bold text-accent">TailwindCSS v3 🎨</h1>
+      <p className="mt-4 text-lg text-secondary">
+        Agora tudo está corrigido! 🚀
+      </p>
+    </div>
+  );
+}
