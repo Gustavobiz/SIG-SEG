fbdc87b58d6cea3f3c6143105e48108a86cf05b5
Gustavobiz <gustavobernardes05@hotmail.com>
2025-03-14 04:52:25 -0300

    fix: denunciaServer com o botão modificado

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,10 +1,7 @@
 export default function Home() {
   return (
-    <div className="flex flex-col items-center justify-center min-h-screen bg-primary text-white">
-      <h1 className="text-4xl font-bold text-accent">TailwindCSS v3 🎨</h1>
-      <p className="mt-4 text-lg text-secondary">
-        Agora tudo está corrigido! 🚀
-      </p>
+    <div className="">
+      <h1 className="">Bem vindo ao SIG-SEG</h1>
     </div>
   );
 }
